b6ce092d8fcb29ed849a730b7d36b3d67c09e514
Miguel Peixe <miguel@peixe.co>
2020-07-07 01:41:59 -0300

    ui fix

--- app/client/components/FlexTable.jsx
+++ app/client/components/FlexTable.jsx
@@ -11,6 +11,7 @@ const FlexTable = styled.div`
 const Row = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 0.5rem 0;
   border-bottom: 1px solid rgba(0, 0, 0, 0.05);
   &:last-child {
@@ -19,7 +20,7 @@ const Row = styled.div`
 `;
 
 const Head = styled.div`
-  flex: 0 0 auto;
+  flex: 1 1 100%;
   margin-right: 3rem;
   font-weight: 600;
   color: rgba(0, 0, 0, 0.4);
@@ -28,6 +29,9 @@ const Head = styled.div`
 const Data = styled.div`
   flex: 1 1 100%;
   text-align: right;
+  input {
+    margin: 0;
+  }
 `;
 
 FlexTable.Row = Row;
