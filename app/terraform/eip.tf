8a3c42228f38bb87a8bbdaf0863f5d9ac276cf8d
Miguel Peixe <miguel@peixe.co>
2020-07-12 00:41:57 -0300

    fix eip setup

--- app/terraform/eip.tf
+++ app/terraform/eip.tf
@@ -2,8 +2,12 @@ variable "eip_id" {
   description = "EIP ID for Instance allocation"
   default     = ""
 }
+variable "eip_instance_config" {
+  description = "Instance config source name for EIP association"
+  default     = ""
+}
 resource "aws_eip" "default" {}
 resource "aws_eip_association" "default" {
-  instance_id   = aws_instance[var.instance_config].id
+  instance_id   = aws_instance[var.eip_instance_config].id
   allocation_id = var.eip_id
 }
